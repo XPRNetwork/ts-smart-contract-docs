89efdd7fa30dddd253fd400d4c0acc5aadf922f4
jafri <syedjafri97@gmail.com>
2022-04-15 18:57:05 -0600

    use latest node

--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -22,22 +22,22 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 1,
       children: [
-        { title: "Accounts and Permissions", path: 'accounts-and-permissions' },
-        { title: "Storage", path: 'storage' },
-        { title: "Inline Actions", path: 'inline-actions' },
-        { title: "Notifications", path: 'notifications' },
-        { title: "Testing", path: 'testing' },
-        { title: "Debugging", path: 'debugging' },
+        { title: "Accounts and Permissions", path: '/accounts-and-permissions' },
+        { title: "Storage", path: '/storage' },
+        { title: "Inline Actions", path: '/inline-actions' },
+        { title: "Notifications", path: '/notifications' },
+        { title: "Testing", path: '/testing' },
+        { title: "Debugging", path: '/debugging' },
         {
           title: 'Language Specifics',
           collapsable: true,
           sidebarDepth: 0,
           children: [
-            { title: "Concepts", path: 'concepts' },
-            { title: "Number Types", path: 'types' },
-            { title: "Globals", path: 'globals' }
+            { title: "Concepts", path: '/concepts' },
+            { title: "Number Types", path: '/types' },
+            { title: "Globals", path: '/globals' }
           ]
-        },
+        }
       ]
     },
     {
@@ -45,10 +45,10 @@ function getDefaultSidebar() {
       collapsable: true,
       sidebarDepth: 1,
       children: [
-        { title: "Web", path: 'client-sdks/web' },
-        { title: "React Native", path: 'client-sdks/react-native'  },
-        { title: "Kotlin", path: 'client-sdks/kotlin'  },
-        { title: "Swift", path: 'client-sdks/swift' },
+        { title: "Web", path: '/client-sdks/web' },
+        { title: "React Native", path: '/client-sdks/react-native'  },
+        { title: "Kotlin", path: '/client-sdks/kotlin'  },
+        { title: "Swift", path: '/client-sdks/swift' },
       ]
     },
     {
@@ -56,8 +56,8 @@ function getDefaultSidebar() {
       collapsable: true,
       sidebarDepth: 1,
       children: [
-        { title: 'Overview', path: 'examples' },
-        { title: 'Built with Proton', path: 'built-with-proton' },
+        { title: 'Overview', path: '/examples' },
+        { title: 'Built with Proton', path: '/built-with-proton' },
       ]
     },
     {
@@ -65,11 +65,11 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 1,
       children: [
-        { title: 'Authentication', path: 'api/authentication' },
-        { title: 'Assert', path: 'api/assert' },
-        { title: 'Blockchain Time', path: 'api/currentTime' },
-        { title: 'Cryptography', path: 'api/cryptography' },
-        { title: 'Print', path: 'api/print' },
+        { title: 'Authentication', path: '/api/authentication' },
+        { title: 'Assert', path: '/api/assert' },
+        { title: 'Blockchain Time', path: '/api/currentTime' },
+        { title: 'Cryptography', path: '/api/cryptography' },
+        { title: 'Print', path: '/api/print' },
 
       ]
     },
@@ -78,41 +78,41 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 0,
       children: [
-        { title: 'Name', path: 'classes/Name' },
-        { title: 'Symbol', path: 'classes/Symbol' },
-        { title: 'ExtendedSymbol', path: 'classes/ExtendedSymbol' },
-        { title: 'Asset', path: 'classes/Asset' },
-        { title: 'ExtendedAsset', path: 'classes/ExtendedAsset' },
-        { title: 'Action', path: 'classes/Action' },
-        { title: 'PermissionLevel', path: 'classes/PermissionLevel' },
-        { title: 'TableStore', path: 'classes/TableStore' },
+        { title: 'Name', path: '/classes/Name' },
+        { title: 'Symbol', path: '/classes/Symbol' },
+        { title: 'ExtendedSymbol', path: '/classes/ExtendedSymbol' },
+        { title: 'Asset', path: '/classes/Asset' },
+        { title: 'ExtendedAsset', path: '/classes/ExtendedAsset' },
+        { title: 'Action', path: '/classes/Action' },
+        { title: 'PermissionLevel', path: '/classes/PermissionLevel' },
+        { title: 'TableStore', path: '/classes/TableStore' },
         {
           title: 'Keys',
           collapsable: true,
           children: [
-            { title: "PublicKey", path: 'classes/keys/PublicKey' },
-            { title: "ECCPublicKey", path: 'classes/keys/ECCPublicKey' },
-            { title: "WebAuthNPublicKey", path: 'classes/keys/WebAuthNPublicKey' }
+            { title: "PublicKey", path: '/classes/keys/PublicKey' },
+            { title: "ECCPublicKey", path: '/classes/keys/ECCPublicKey' },
+            { title: "WebAuthNPublicKey", path: '/classes/keys/WebAuthNPublicKey' }
           ]
         },
-        { title: 'Signature', path: 'classes/Signature' },
+        { title: 'Signature', path: '/classes/Signature' },
         {
           title: 'Time',
           collapsable: true,
           children: [
-            { title: "Microseconds", path: 'classes/time/Microseconds' },
-            { title: "TimePoint", path: 'classes/time/TimePoint' },
-            { title: "TimePointSec", path: 'classes/time/TimePointSec' },
-            { title: "BlockTimestamp", path: 'classes/time/BlockTimestamp' },
+            { title: "Microseconds", path: '/classes/time/Microseconds' },
+            { title: "TimePoint", path: '/classes/time/TimePoint' },
+            { title: "TimePointSec", path: '/classes/time/TimePointSec' },
+            { title: "BlockTimestamp", path: '/classes/time/BlockTimestamp' },
           ]
         },
         {
           title: 'Checksum',
           collapsable: true,
           children: [
-            { title: "Checksum160", path: 'classes/checksum/Checksum160' },
-            { title: "Checksum256", path: 'classes/checksum/Checksum256' },
-            { title: "Checksum512", path: 'classes/checksum/Checksum512' },
+            { title: "Checksum160", path: '/classes/checksum/Checksum160' },
+            { title: "Checksum256", path: '/classes/checksum/Checksum256' },
+            { title: "Checksum512", path: '/classes/checksum/Checksum512' },
           ]
         }
       ]
