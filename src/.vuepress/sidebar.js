6bf1c6194df4f4f9a44a56ca2e3d442395fb15fb
jafri <syedjafri97@gmail.com>
2022-04-15 18:40:01 -0600

    try without slash

--- src/.vuepress/sidebar.js
+++ src/.vuepress/sidebar.js
@@ -7,13 +7,13 @@ function getDefaultSidebar() {
   return [
     {
       title: "Introduction",
-      path: '/introduction',
+      path: 'introduction',
       collapsable: false,
       sidebarDepth: 0,
     },
     {
       title: "Getting started",
-      path: '/getting-started',
+      path: 'getting-started',
       collapsable: false,
       sidebarDepth: 1,
     },
@@ -22,20 +22,20 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 1,
       children: [
-        { title: "Accounts and Permissions", path: '/accounts-and-permissions' },
-        { title: "Storage", path: '/storage' },
-        { title: "Inline Actions", path: '/inline-actions' },
-        { title: "Notifications", path: '/notifications' },
-        { title: "Testing", path: '/testing' },
-        { title: "Debugging", path: '/debugging' },
+        { title: "Accounts and Permissions", path: 'accounts-and-permissions' },
+        { title: "Storage", path: 'storage' },
+        { title: "Inline Actions", path: 'inline-actions' },
+        { title: "Notifications", path: 'notifications' },
+        { title: "Testing", path: 'testing' },
+        { title: "Debugging", path: 'debugging' },
         {
           title: 'Language Specifics',
           collapsable: true,
           sidebarDepth: 0,
           children: [
-            { title: "Concepts", path: '/concepts' },
-            { title: "Number Types", path: '/types' },
-            { title: "Globals", path: '/globals' }
+            { title: "Concepts", path: 'concepts' },
+            { title: "Number Types", path: 'types' },
+            { title: "Globals", path: 'globals' }
           ]
         },
       ]
@@ -45,10 +45,10 @@ function getDefaultSidebar() {
       collapsable: true,
       sidebarDepth: 1,
       children: [
-        { title: "Web", path: '/client-sdks/web' },
-        { title: "React Native", path: '/client-sdks/react-native'  },
-        { title: "Kotlin", path: '/client-sdks/kotlin'  },
-        { title: "Swift", path: '/client-sdks/swift' },
+        { title: "Web", path: 'client-sdks/web' },
+        { title: "React Native", path: 'client-sdks/react-native'  },
+        { title: "Kotlin", path: 'client-sdks/kotlin'  },
+        { title: "Swift", path: 'client-sdks/swift' },
       ]
     },
     {
@@ -56,8 +56,8 @@ function getDefaultSidebar() {
       collapsable: true,
       sidebarDepth: 1,
       children: [
-        { title: 'Overview', path: '/examples' },
-        { title: 'Built with Proton', path: '/built-with-proton' },
+        { title: 'Overview', path: 'examples' },
+        { title: 'Built with Proton', path: 'built-with-proton' },
       ]
     },
     {
@@ -65,11 +65,11 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 1,
       children: [
-        { title: 'Authentication', path: '/api/authentication' },
-        { title: 'Assert', path: '/api/assert' },
-        { title: 'Blockchain Time', path: '/api/currentTime' },
-        { title: 'Cryptography', path: '/api/cryptography' },
-        { title: 'Print', path: '/api/print' },
+        { title: 'Authentication', path: 'api/authentication' },
+        { title: 'Assert', path: 'api/assert' },
+        { title: 'Blockchain Time', path: 'api/currentTime' },
+        { title: 'Cryptography', path: 'api/cryptography' },
+        { title: 'Print', path: 'api/print' },
 
       ]
     },
@@ -78,46 +78,46 @@ function getDefaultSidebar() {
       collapsable: false,
       sidebarDepth: 0,
       children: [
-        { title: 'Name', path: '/classes/Name' },
-        { title: 'Symbol', path: '/classes/Symbol' },
-        { title: 'ExtendedSymbol', path: '/classes/ExtendedSymbol' },
-        { title: 'Asset', path: '/classes/Asset' },
-        { title: 'ExtendedAsset', path: '/classes/ExtendedAsset' },
-        { title: 'Action', path: '/classes/Action' },
-        { title: 'PermissionLevel', path: '/classes/PermissionLevel' },
+        { title: 'Name', path: 'classes/Name' },
+        { title: 'Symbol', path: 'classes/Symbol' },
+        { title: 'ExtendedSymbol', path: 'classes/ExtendedSymbol' },
+        { title: 'Asset', path: 'classes/Asset' },
+        { title: 'ExtendedAsset', path: 'classes/ExtendedAsset' },
+        { title: 'Action', path: 'classes/Action' },
+        { title: 'PermissionLevel', path: 'classes/PermissionLevel' },
         {
           title: 'TableStore',
-          path: '/classes/TableStore.md'
+          path: 'classes/TableStore.md'
         },
         {
           title: 'Keys',
           collapsable: true,
           children: [
-            { title: "PublicKey", path: '/classes/keys/PublicKey' },
-            { title: "ECCPublicKey", path: '/classes/keys/ECCPublicKey' },
-            { title: "WebAuthNPublicKey", path: '/classes/keys/WebAuthNPublicKey' }
+            { title: "PublicKey", path: 'classes/keys/PublicKey' },
+            { title: "ECCPublicKey", path: 'classes/keys/ECCPublicKey' },
+            { title: "WebAuthNPublicKey", path: 'classes/keys/WebAuthNPublicKey' }
           ]
         },
-        { title: 'Signature', path: '/classes/Signature' },
+        { title: 'Signature', path: 'classes/Signature' },
         {
           title: 'Time',
           collapsable: true,
           children: [
             {
               title: "Microseconds",
-              path: '/classes/time/Microseconds'
+              path: 'classes/time/Microseconds'
             },
             {
               title: "TimePoint",
-              path: '/classes/time/TimePoint'
+              path: 'classes/time/TimePoint'
             },
             {
               title: "TimePointSec",
-              path: '/classes/time/TimePointSec'
+              path: 'classes/time/TimePointSec'
             },
             {
               title: "BlockTimestamp",
-              path: '/classes/time/BlockTimestamp'
+              path: 'classes/time/BlockTimestamp'
             },
           ]
         }
